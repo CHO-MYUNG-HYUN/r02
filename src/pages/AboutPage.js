112202711a2ff6e0219fce6a2073da9b3819ffab
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-07-05 17:11:57 +0900

    0705 Router

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import TestLayout from "../layouts/BasicLayout";
+
+const AboutPage = () => {
+  return (  
+    <TestLayout>
+      <h2>About Page</h2>
+    </TestLayout>
+  );
+}
+ 
+export default AboutPage;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import TestLayout from "../layouts/BasicLayout";
+
+const MainPage = () => {
+  return (  
+    <TestLayout>
+      <h2>Main Page</h2>
+    </TestLayout>
+  );
+}
+ 
+export default MainPage;
