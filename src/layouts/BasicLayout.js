9c9b2497d24544ae968f3c10a2aefb360acfd498
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-07-14 18:19:58 +0900

    0713 Rounter FileUpload

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -4,7 +4,7 @@ const BasicLayout = ({ children }) => {
   return (
     <div className="container mx-auto min-m-[1280px] bg-no-repeat bg-cover bg-bottom
     bg-[url('https://cdn.pixabay.com/photo/2022/03/13/12/00/hydrangeas-7065907_1280.jpg')]
-    
+    font-custom
     ">
       <div>
         <SampleNav></SampleNav>
