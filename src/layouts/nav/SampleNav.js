84a8b4c3f9fa2ccf136044d681e401ce2e597779
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-07-14 18:20:15 +0900

    0714 Redux

--- src/layouts/nav/SampleNav.js
+++ src/layouts/nav/SampleNav.js
@@ -5,10 +5,11 @@ import LoginNav from "./LoginNav";
 const SampleNav = () => {
 
   const todoArr = useSelector(state => state.todo)
-
+  // https://cdn.pixabay.com/photo/2014/07/21/11/51/rose-398576_640.png
 
   return (
-    <div className="flex m-4 p-4  text-yellow-300 font-extrabold justify-center">
+    <div className="flex m-4 p-4  text-yellow-200 font-extrabold justify-center">
+      {/* <div className="w-20 h-20 bg-[url('https://cdn.pixabay.com/photo/2014/07/21/11/51/rose-398576_640.png')] -ml-36"></div> */}
       <div className="m-4 text-4xl ">
         <Link to={"/"}>Main</Link>
         <span className="bg-red-500 font-extrabold">{todoArr.length}</span>
