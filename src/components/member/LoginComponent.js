a19af57eab34e25f43bd13b1dc9fa95c689f1919
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-07-18 18:24:00 +0900

    0717 Redux Login Cart

--- src/components/member/LoginComponent.js
+++ src/components/member/LoginComponent.js
@@ -21,25 +21,25 @@ const LoginComponent = () => {
   console.log("e: ", errorMsg)
 
   return (
-    <div className="font-bold">
+    <div className="font-bold text-2xl text-yellow-300">
       <div className="text-3xl bg-red-500">
         {loginState.loading ? "로그인중..." : ""}
       </div>
 
       {errorMsg ? <div className="text-3xl bg-red-500">이메일과 패스워드를 다시 확인해 주세요.</div> : <></>}
 
-      <div>
-        <label>Email</label>
-        <input
+      <div className="w-96 m-2 p-2 ">
+        <label>Email : </label>
+        <input className="ml-[11%] text-black bg-transparent backdrop-blur-md border-2 rounded-lg "
           type="text"
           name="email"
           value={loginInfo.email}
           onChange={() => { }}
         ></input>
       </div>
-      <div>
-        <label>Password</label>
-        <input
+      <div className="w-96 m-2 p-2">
+        <label>Password : </label>
+        <input className="ml-[3%] text-black bg-transparent backdrop-blur-md border-2 rounded-lg"
           type="password"
           name="pw"
           value={loginInfo.pw}
@@ -47,7 +47,8 @@ const LoginComponent = () => {
         ></input>
       </div>
       <div>
-        <button onClick={() => dispatch(postLoginThunk(loginInfo))}>Login</button>
+        <button className="text-3xl text-white m-2 p-2" 
+        onClick={() => dispatch(postLoginThunk(loginInfo))}> Login </button>
       </div>
     </div>
   );
